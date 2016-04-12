f2c7ed7e3f2d9cdb3cc7bc3b42cb121f0f5551b3
cgoss <colin.goss@gmail.com>
2016-04-11 21:01:41 -0700

    Updated sample tests

--- elastictest.js
+++ elastictest.js
@@ -1,10 +1,11 @@
 var elasticsearch = require('elasticsearch');
 
 var elasticClient = new elasticsearch.Client({
-    host: '127.0.0.1:9200',
+    //host: '127.0.0.1:9200',
+    host: '192.168.1.104:9200',
     sniffOnStart: true,
     apiVersion:'2.2',
-    log: 'trace'
+    log: 'info'
 });
 
 console.log("ping elastic");
@@ -24,21 +25,37 @@ elasticClient.ping({
 console.log("quick search");
 
 elasticClient.search({
-  type:'search'
+  type:'search',
+   'index-pattern': "/settings/objects/savedSearches/"
 }).then(function (body) {
-  var searches=[];
-  for(var result in hits)
-  {
-    searches.push(result);
+  var ii = 0, hits_in, hits_out = [];
+  hits_in = (body.hits || {}).hits || [];
+  //deferred.resolve(result.hits);
+  var result=[];
+  for(; ii < hits_in.length; ii++) {
+      result.push(JSON.stringify(hits_in[ii]._source.kibanaSavedObjectMeta.searchSourceJSON));
   }
-  console.log(searches);
+console.log(result);
+
+//   var searches=[];
+//   var inHits = body.hits.hits;
+//   for(var result in inHits)
+//   {
+//     searches.push(JSON.stringify(result));
+//     console.log(JSON.stringify(result));
+//   }
+//   console.log(JSON.stringify(searches));
+//   var hits = body.hits.hits;
+// //  res.send(hits[0]._source.kibanaSavedObjectMeta.searchSourceJSON);
+//   console.log(hits[0]);
 }, function (error) {
   console.trace(error.message);
 });
 
 
 
-
+// 'search': '/discover',
+//        'index-pattern': '/settings/objects/savedSearches/' + $route.current.params.id
 //
 // function getQuery($queryName){
 //   console.log($queryName);
