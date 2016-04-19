9a6447da49afaa77441905fc9c091b62baf9b344
cgoss <colin.goss@gmail.com>
2016-04-19 00:28:25 -0700

    Added send status and next to method calls

--- controllers/notificationcontroller.js
+++ controllers/notificationcontroller.js
@@ -76,9 +76,13 @@ module.exports.RegisterNotification= function(req,res,next)
   var alertInfo = req.body.notificationName;
   console.log(JSON.stringify(alertInfo));
 //  notificationEngine.RegisterNotification(alertInfo);
+  res.sendStatus(true);
+  next();
 }
 module.exports.UnregisterNotification= function(req,res,next)
 {
   var notificationName = req.body.notificationName;
   notificationEngine.UnregisterNotification(notificationName);
+  res.sendStatus(true);
+  next();
 }
