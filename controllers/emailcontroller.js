da57c392f00f2055bd0958d0abdb7c20d4466908
cgoss <colin.goss@gmail.com>
2016-04-23 22:21:21 -0700

    updated content type

--- controllers/emailcontroller.js
+++ controllers/emailcontroller.js
@@ -121,7 +121,8 @@ var messagetext = "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN
      to:      alertInfo.notifyEmail,
      subject: "Alert: " + alertInfo.notificationName,
      text: messagetext,
-     content: "text/html; charset=UTF-8"
+     content: "text/html; charset=UTF-8",
+     alternative = true,
      // attachment:
      // [
      //    {data:"<html>i <i>hope</i> this works!</html>", alternative:alertInfo.htmlEmail=='true'},
