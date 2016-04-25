3ecc0b351093fbfab682c46d4fae517a050b416d
cgoss <colin.goss@gmail.com>
2016-04-24 22:43:25 -0700

    updated table

--- controllers/emailcontroller.js
+++ controllers/emailcontroller.js
@@ -112,7 +112,8 @@ switch(alertInfo.thresholdType){
 }
 
 
-var messagetext = "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>"
+var messagetext =
+                "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>"
                 +"<html xmlns='http://www.w3.org/1999/xhtml'>"
                 +"<head>"
                 +"<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />"
@@ -134,7 +135,7 @@ var messagetext = "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN
                 "</td></tr>" +
                 "<tr><td><strong>Description:</strong></td><td>"
                 + alertInfo.notificationDescription +
-                "</td></tr>" +
+                "</td></tr></table>" +
                 +"</head>"
                 +"</html>";
 
