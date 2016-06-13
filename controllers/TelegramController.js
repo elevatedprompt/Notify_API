b0371a61dfea60c3aeb47271b33a2e09f191285c
cgoss <colin.goss@gmail.com>
2016-06-12 22:54:07 -0700

    update to message string

--- controllers/TelegramController.js
+++ controllers/TelegramController.js
@@ -6,35 +6,35 @@ var unirest = require('unirest');
 module.exports.SendTelegramEvent = function(alertInfo,result,triggerTime){
   logEvent("Telegram method fired");
   var messagetext =
-    "#Alert Notification#\n" +
+    "#Alert Notification#" +
       +"##"  + alertInfo.notificationName + " @ " + triggerTime.toISOString()
                                                           .replace(/T/, ' ')
-                                                          .replace(/\..+/, '') + "##\n" +
-          "A conditional search trigger has been met.\n" +
-          "Search Name: " +alertInfo.selectedSearch + "\n" +
+                                                          .replace(/\..+/, '') + "##" +
+          "A conditional search trigger has been met." +
+          "Search Name: " +alertInfo.selectedSearch + "" +
           "Condition:" +
           alertInfo.thresholdType + " "
-          + alertInfo.thresholdCount + " in " + alertInfo.timeValue + " " + alertInfo.timeframe + "\n" +
+          + alertInfo.thresholdCount + " in " + alertInfo.timeValue + " " + alertInfo.timeframe + "" +
           "" +
                   "Result Count:"
                   + result.total +
-                  "\n" +
+                  "" +
                   "Description:"
                   + alertInfo.notificationDescription +
                   "";
                 //  logEvent('https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage');
                 console.log(messagetext);
-                  var methodCall ='https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage?text='+ messagetext +'&chat_id=' +global.telegramChatId ;
+                  var methodCall ='https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage?chat_id=' +global.telegramChatId + '&text='+ messagetext;
 
-                //   unirest.post(methodCall)
-                //   .headers({'Accept': 'application/json','Content-Type': 'application/json'})
-                // //  .field('chat_id',global.telegramChatId)
-                // //  .field('text','test')
-                //   //.send({ form: { chat_id : global.telegramChatId,text : 'test'} })
-                //   //JSON.stringify(notification))
-                //   .end(function (response) {
-                //     logEvent(response);
-                //   });
+                  unirest.post(methodCall)
+                  .headers({'Accept': 'application/json','Content-Type': 'application/json'})
+                //  .field('chat_id',global.telegramChatId)
+                //  .field('text','test')
+                  //.send({ form: { chat_id : global.telegramChatId,text : 'test'} })
+                  //JSON.stringify(notification))
+                  .end(function (response) {
+                    logEvent(response);
+                  });
 
                   // req.post('https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage',
               		// 	{ form: { chat_id : global.telegramChatId,text : messagetext} },
