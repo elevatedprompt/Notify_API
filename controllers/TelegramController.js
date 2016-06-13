02d196724e330ee563034713c5aff1bcc7e1bdae
cgoss <colin.goss@gmail.com>
2016-06-12 23:03:19 -0700

    test

--- controllers/TelegramController.js
+++ controllers/TelegramController.js
@@ -6,7 +6,7 @@ var unirest = require('unirest');
 module.exports.SendTelegramEvent = function(alertInfo,result,triggerTime){
   logEvent("Telegram method fired");
   var messagetext =
-     'Alert Notification' +
+     'Alert Notification\n' +
        +''  + alertInfo.notificationName;// + " @ " + triggerTime.toISOString()
                                             //              .replace(/T/, ' ')
                                             //              .replace(/\..+/, '') + "##";// +
