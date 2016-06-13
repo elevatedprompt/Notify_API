dce78a31bfd72a22d15fd5987676c95fe938853a
cgoss <colin.goss@gmail.com>
2016-06-12 22:06:09 -0700

    added logging around telegram

--- controllers/TelegramController.js
+++ controllers/TelegramController.js
@@ -3,7 +3,7 @@ var req = require('request');
 
 
 module.exports.SendTelegramEvent = function(alertInfo,result,triggerTime){
-
+  logEvent("Telegram method fired");
   var messagetext =
                   "<table><tr><td colspan='2'><strong>A conditional search trigger has been met.</strong></td></tr><tr><td colspan='2'>&nbsp;</td></tr>"+
                   "<tr><td><strong>Notification Name:</strong></td><td>"
@@ -24,13 +24,20 @@ module.exports.SendTelegramEvent = function(alertInfo,result,triggerTime){
                   "<tr><td><strong>Description:</strong></td><td>"
                   + alertInfo.notificationDescription +
                   "</td></tr></table>";
-
+                  logEvent('https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage');
                   req.post('https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage',
               			{ form: { chat_id : global.telegramChatId,text : messagetext} },
               			function (error, response, body) {
+                      logEvent("Sent message to Telegram");
               				if (error) {
               					console.log(body)
               				}
               			}
               		);
 }
+
+function logEvent(message){
+                            if(global.tracelevel == 'debug'){
+                                                              console.log(message);
+                                                              }
+                          }
