5ded21351b7e9e2ada0b039a81703bd6df0a4249
cgoss <colin.goss@gmail.com>
2016-06-12 23:07:47 -0700

    test

--- controllers/TelegramController.js
+++ controllers/TelegramController.js
@@ -6,8 +6,8 @@ var unirest = require('unirest');
 module.exports.SendTelegramEvent = function(alertInfo,result,triggerTime){
   logEvent("Telegram method fired");
   var messagetext =
-     "Alert Notification\n" +
-        + alertInfo.notificationName.toString();// + " @ " + triggerTime.toISOString()
+     "Alert Notification\n " +
+         alertInfo.notificationName.toString();// + " @ " + triggerTime.toISOString()
                                             //              .replace(/T/, ' ')
                                             //              .replace(/\..+/, '') + "##";// +
           // "A conditional search trigger has been met." +
