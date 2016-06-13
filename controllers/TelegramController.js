517df5d415e5cfe690db5c4a0503e403e889b084
cgoss <colin.goss@gmail.com>
2016-06-12 22:27:31 -0700

    added telegram test

--- controllers/TelegramController.js
+++ controllers/TelegramController.js
@@ -5,33 +5,33 @@ var unirest = require('unirest');
 
 module.exports.SendTelegramEvent = function(alertInfo,result,triggerTime){
   logEvent("Telegram method fired");
-  var messagetext =
-                  "<table><tr><td colspan='2'><strong>A conditional search trigger has been met.</strong></td></tr><tr><td colspan='2'>&nbsp;</td></tr>"+
-                  "<tr><td><strong>Notification Name:</strong></td><td>"
-                  + alertInfo.notificationName + " @ " + triggerTime.toISOString()
-                                                                    .replace(/T/, ' ')
-                                                                    .replace(/\..+/, '') +
-                  "</td></tr>" +
-                  "<tr><td><strong>Search Name:</strong></td><td>"
-                  + alertInfo.selectedSearch +
-                  "</td></tr>" +
-                  "<tr><td><strong>Condition:</strong></td><td>" +
-                  alertInfo.thresholdType + " "
-                  + alertInfo.thresholdCount + " in " + alertInfo.timeValue + " " + timeframe + "\n" +
-                  "</td></tr>" +
-                  "<tr><td><strong>Result Count:</strong></td><td>"
-                  + result.total +
-                  "</td></tr>" +
-                  "<tr><td><strong>Description:</strong></td><td>"
-                  + alertInfo.notificationDescription +
-                  "</td></tr></table>";
-                  logEvent('https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage');
+  // var messagetext =
+  //                 "<table><tr><td colspan='2'><strong>A conditional search trigger has been met.</strong></td></tr><tr><td colspan='2'>&nbsp;</td></tr>"+
+  //                 "<tr><td><strong>Notification Name:</strong></td><td>"
+  //                 + alertInfo.notificationName + " @ " + triggerTime.toISOString()
+  //                                                                   .replace(/T/, ' ')
+  //                                                                   .replace(/\..+/, '') +
+  //                 "</td></tr>" +
+  //                 "<tr><td><strong>Search Name:</strong></td><td>"
+  //                 + alertInfo.selectedSearch +
+  //                 "</td></tr>" +
+  //                 "<tr><td><strong>Condition:</strong></td><td>" +
+  //                 alertInfo.thresholdType + " "
+  //                 + alertInfo.thresholdCount + " in " + alertInfo.timeValue + " " + alertInfo.timeframe + "\n" +
+  //                 "</td></tr>" +
+  //                 "<tr><td><strong>Result Count:</strong></td><td>"
+  //                 + result.total +
+  //                 "</td></tr>" +
+  //                 "<tr><td><strong>Description:</strong></td><td>"
+  //                 + alertInfo.notificationDescription +
+  //                 "</td></tr></table>";
+                //  logEvent('https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage');
 
                   var methodCall ='https://api.telegram.org/'+global.telegramAPIKey +'/sendMessage';
 
                   unirest.post(methodCall)
                 //  .headers({'Accept': 'application/json','Content-Type': 'application/json'})
-                  .send({ form: { chat_id : global.telegramChatId,text : messagetext} })
+                  .send({ form: { chat_id : global.telegramChatId,text : 'test'} })
                   //JSON.stringify(notification))
                   .end(function (response) {
                     logEvent(response);
