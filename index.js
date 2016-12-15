0c9b1b05ae3f6db0266db672a0335db8f408afbd
cgoss <colin.goss@gmail.com>
2016-12-14 21:37:57 -0800

    Load new configuration values.

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ logEvent(configuration);
 
 global.UbuntuV16 = configuration.UbuntuV16;
 global.tracelevel =   configuration.tracelevel;
+global.loggingDirectory =   configuration.loggingDirectory;
+global.notificationDirectory = configuration.notificationDirectory;
 global.elastichost =  configuration.elastichost;
 global.notificationDirectory = configuration.notificationDirectory;
 global.emailConfiguration = configuration.emailConfiguration;
