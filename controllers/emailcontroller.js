e6be4d320746fe78f4d41a45ba907cd34c344207
cgoss <colin.goss@gmail.com>
2016-12-17 02:13:09 -0800

    ++

--- controllers/emailcontroller.js
+++ controllers/emailcontroller.js
@@ -91,10 +91,10 @@ module.exports.SendEventMail = function(alertInfo,result,triggerTime){
                                                             logEvent(JSON.stringify(result));
                                                             logEvent('Split notifyData');
 
-                                                            logEvent(alertInfo.notifyData.replace('{','').replace('}','').split('.'));
+                                                            //logEvent(alertInfo.notifyData.replace('{','').replace('}','').split('.'));
                                                             var tokens = alertInfo.notifyData.replace('{','').replace('}','').split('.');
-                                                            logEvent("tokens" + tokens);
-                                                            logEvent("tokens" + JSON.stringify(tokens));
+                                                            //logEvent("tokens" + tokens);
+                                                            //logEvent("tokens" + JSON.stringify(tokens));
                                                             for(hit in result.hits){
                                                               logEvent('Parse results');
                                                               logEvent(JSON.stringify(tokens));
@@ -103,7 +103,7 @@ module.exports.SendEventMail = function(alertInfo,result,triggerTime){
                                                                 logEvent("data: " + JSON.stringify(temp));
                                                                 logEvent("hit :" + JSON.stringify(hit));
                                                                 logEvent("t :" + JSON.stringify(hit));
-                                                                temp = hit[token.toString()];
+                                                                temp = temp[token.toString()];
                                                                 logEvent("Walk out the tokens");
                                                                 logEvent("token: " + JSON.stringify(token));
                                                                 logEvent("value: " + JSON.stringify(temp));
