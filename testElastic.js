c0c0184510e80c8bd496cdd9023b5f6afeb94bf4
cgoss <colin.goss@gmail.com>
2016-12-23 19:48:15 -0800

    added test elastic search client.

---
+++ testElastic.js
@@ -0,0 +1,51 @@
+var elasticsearch = require('elasticsearch');
+
+var elasticClient = new elasticsearch.Client({
+                                                host: '127.0.0.1:9200',
+                                                sniffOnStart: true,
+                                                apiVersion:'2.2',
+                                                log: 'error'
+                                            });
+
+
+ function pingCluster(){
+                                                      elasticClient.ping({
+                                                                          requestTimeout: 30000,
+                                                                          // undocumented params are appended to the query string
+                                                                          hello: "elasticsearch"
+                                                                        }, function (error) {
+                                                                          if (error) {
+                                                                            console.error('elasticsearch cluster is down!');
+                          //                                                  res.sendStatus(false);
+                        //                                                    next();
+                                                                          } else {
+                                                                            logEvent('All is well');
+                            //                                                res.sendStatus(true);
+                      //                                                      next();
+                                                                          }
+                                                                        });
+                                                    }
+
+function listSearches(){
+                 logEvent('ElasticController=>ListSearches');
+                 elasticClient.search({
+                                         type:'search',
+                                         name:''
+                                       }).then(function (body) {
+                                                                 var searches=[];
+                                                                 for(var result in body.hits.hits)
+                                                                 {
+                                                                   searches.push(result);
+                                                                 }
+                                                                 console.info(searches);
+                                                               }, function (error) {
+                                                                 console.error(error);
+                    //                                             traceEvent(error.message);
+                                                               });
+               }
+
+
+
+
+pingCluster();
+listSearches();
